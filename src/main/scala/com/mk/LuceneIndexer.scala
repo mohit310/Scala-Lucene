3e6729fd0e4eb2d122e4a528f040abf773119d59
mohit keswani <mohit.keswani@gmail.com>
2015-06-27 12:20:40 -0400

    modified for Loaner pattern for indexer and reader

--- src/main/scala/com/mk/LuceneIndexer.scala
+++ src/main/scala/com/mk/LuceneIndexer.scala
@@ -22,32 +22,40 @@ class LuceneIndexer {
     methodName.invoke(x).toString
   }
 
+  private def writeIndex(iw: IndexWriter => Unit) = {
+    iw(writer)
+    writer.commit()
+    writer.close()
+  }
+
   def index(x: SampleData, indexField: String) = {
     val doc = new Document()
     val jsString = ObjectToJsonConverter.convert(x)
     doc.add(new TextField("name", getValue(x, indexField), Field.Store.YES))
     doc.add(new StoredField("json", jsString))
-    writer.addDocument(doc)
-    writer.commit()
-    writer.close()
+    writeIndex(indexWriter => indexWriter.addDocument(doc))
   }
 
-  def search(indexField: String, searchCriteria: String): String = {
-    val query = new QueryParser(indexField, analyzer).parse(searchCriteria)
+  private def readSearcher(isearch: IndexSearcher => String) = {
     val indexReader = DirectoryReader.open(directory)
     val searcher = new IndexSearcher(indexReader)
-    val results = searcher.search(query, 100)
-    val hits = results.scoreDocs
-    println("Total hits:" + results.totalHits)
-    results.totalHits match {
-      case 0 =>
+    val data = isearch(searcher)
+    indexReader.close()
+    data
+  }
+
+  def search(indexField: String, searchCriteria: String) = {
+    val query = new QueryParser(indexField, analyzer).parse(searchCriteria)
+    readSearcher(indexSearcher => {
+      val results = indexSearcher.search(query, 100)
+      val hits = results.scoreDocs
+      println("Total hits:" + results.totalHits)
+      if (results.totalHits > 0) {
+        val doc = indexSearcher.doc(hits(0).doc)
+        doc.toString
+      } else
         ""
-      case _ =>
-        val doc = searcher.doc(hits(0).doc)
-        val data = doc.toString
-        indexReader.close()
-        data
-    }
+    })
   }
 
 }
